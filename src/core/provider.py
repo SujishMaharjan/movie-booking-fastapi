8dad57027a66101b498e69b0dcd4c54f1ec6adfd
SujishMaharjan <sujishmaharjan@gmail.com>
2025-05-15 14:31:04 +0545

    removed unneccessary function in security

--- src/core/provider.py
+++ src/core/provider.py
@@ -1,16 +1,3 @@
-from src.modules.auth.interfaces.token_repository import TokenRepository
-# from src.modules.auth.infrastructure.Jwt_token_repository import JwtService
-from src.modules.user.interfaces.user_repository import UserRepository
-# from src.modules.user.infrastructure.user_postgres_repository import PostgresUserRepository
-from src.modules.movie.interfaces.movie_repository import MovieRepository
-# from src.modules.movie.infrastructure.movie_postgres_repository import PostgresMovieRepository
-from src.modules.reserve.interfaces.reserve_repository import ReserveRepository
-# from src.modules.reserve.infrastructure.postgres_reserve_repository import PostgresReserveRepository
-from src.modules.auth.interfaces.password_hasher_repository import PasswordHasher
-# from src.modules.auth.infrastructure.bycrypt_password_hasher import BcryptPasswordHasher
-
-
-
 class Provider:
     def __init__(
             self,
